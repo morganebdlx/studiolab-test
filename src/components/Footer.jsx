dc3cc83bd535d42f03d15a19b3dfac495b03e150
Morgane Bourdillon <mlacroix8@gmail.com>
2025-05-16 17:44:55 +0200

    ajout d'une font

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer>
-      <p>© 2025 Morgane Bourdillon for Studio Lab All rights reserved.</p>
+      <p className="dm-sans">© 2025 Morgane Bourdillon for Studio Lab All rights reserved.</p>
     </footer>
   )
 }
